bdedc67034bc2be098badf992256c89f79ec24e2
Mark Xu <isafesoft@hotmail.com>
2020-03-24 20:08:28 -0400

    stream rest crud

--- src/actions/streams.js
+++ src/actions/streams.js
@@ -1,9 +1,41 @@
-import {STREAMS_CREATE} from "../helper";
+import {STREAMS_CREATE, STREAMS_DELETE, STREAMS_EDIT, STREAMS_FETCH, STREAMS_FETCH_ALL} from "../helper";
+import streams from '../api/streams'
+// redux-thunk dispatch
+// redux-thunk type action creator//
+// export const streamCreate = (formValues) => {
+//    return async (dispatch) => {
+//        let response = await streams.post(formValues)
+//        dispatch({type: formValues, payload: {}})
+//     }
+// }
 
-export const streamCreate = (formValues) => {
+//
+const restObj = '/streams'
 
-    return {
-        type: STREAMS_CREATE,
-
-    }
+export const streamCreate = formValues => async dispatch => {
+    let response = await streams.post(restObj, formValues)
+    dispatch({type: STREAMS_CREATE, payload: response.data})
 }
+
+export const streamEdit= (id, formValue) => async dispatch => {
+    let response = await streams.put(`${restObj}/${id}`, {formValue})
+    dispatch({type: STREAMS_EDIT, payload: response.data})
+}
+
+export const streamFetch = id => async dispatch => {
+    let response = await streams.get(`${restObj}/${id}`)
+    dispatch({type: STREAMS_FETCH, payload: response.data})
+}
+
+export const streamFetchAll = () => async dispatch => {
+    let response = await streams.get(restObj)
+    dispatch({type: STREAMS_FETCH_ALL, payload: response.data})
+}
+
+export const streamDelete = id => async dispatch => {
+    let response = await streams.delete(`${restObj}/${id}`)
+    dispatch({type: STREAMS_DELETE, payload: response.data})
+}
+
+
+
